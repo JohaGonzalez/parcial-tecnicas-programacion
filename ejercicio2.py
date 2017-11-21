0da885fad5c07a928a11c45a86c849fc56cdf0fa
JohaGonzalez <joha18_29@hotmail.com>
2017-11-21 20:23:19 -0300

    Se suben los archivos de prueba, junto con la resolución del ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,57 +1,82 @@
-def batallaDeBotes (columna,posicionADisparar ):
-    mapa = []
-    for i in range (len (columna)):
-        if (columna [i] != ".") or (columna [i] != "b"):
-            return []
-    for fila in columna:
-        if "b" in fila:
-            posicion = 0
-            for caracter in fila:
 
-                if caracter == "b":
-                    botes = (columna.index (fila)+ 1, posicion+1)
-                    posicion = posicion + 1
-                else:
-                    posicion = posicion + 1
-                    continue
-                mapa.append (botes)
-    for disparo in posicionADisparar:
-        if disparo in mapa:
-            mapa.remove (disparo)
-    return mapa
+def mapaInvalido(mapa):
 
-def funcionResto1(tuplaAux):
-    tuplaAux [0] = t [0] - 1
-    tuplaAux[1] = t[1] - 1
-    return tuplaAux
+    if mapa == []:
+        return True
 
-def funcionSuma1(tuplaAux1):
-    tuplaAux1 [0] = t [0] + 1
-    tuplaAux1 [1] = t[1] + 1
-    return tuplaAux1
+    longitudFilas = []
 
-def tuplaAuxiliar ():
-    for n in range (len(posicionesDeDisparosDePrueba)-1):
-        tt= posicionesDeDisparosDePrueba [m]
-        t= funcionResto1(tt)
-        if m [tuplaAuxiliar(0)] [tuplaAuxiliar(1)] == "b":
-            m [tuplaAuxiliar(0)] [tuplaAuxiliar(1)]== "."
-    return tuplaAuxiliar
+    for index, fila in enumerate(mapa):
 
-def sobrevivientes ():
-    listaSobrevivientes = []
-    for i in range (len (m)):
-        for j in range (len m [i]):
-        if (m [i] [j]== "b"):
-            t = (i,j)
-        tAuxiliar = funcionSuma1(t)
-    listaSobrevivientes.append (tAuxiliar)
-    return sobrevivientes
+        longitudFilas.append(len(fila))
+
+        if longitudFilas[index] == 0:
+            return True
+
+        if index > 0:
+            if longitudFilas[index] != longitudFilas[index-1]:
+                return True
+
+        for letra in fila:
+            if letra != '.' and letra != 'b':
+                return True
+
+    return False
 
 
+def convertirMapaAMapaBooleano(mapa):
+
+    mapaBooleano =[]
+
+    for fila in mapa:
+
+        filaBooleana = []
+
+        for letra in fila:
+            if letra == '.':
+                filaBooleana.append(False)
+            else:
+                filaBooleana.append(True)
+
+        mapaBooleano.append(filaBooleana)
+
+    return mapaBooleano
+
+"""
+PRE:
+POST: Modifica el mapa pasado por parametro
+"""
+def disparAlMapa(mapa,posicionesADisparar):
+
+    for disparo in posicionesADisparar:
+        fila = disparo[0]-1
+        columna = disparo[1]-1
+        mapa[fila][columna] = False
+
+def obtenerPosicionesBarcosNoHundidos(mapa):
+
+    posiciones = []
+
+    for indexFila, fila in enumerate(mapa):
+        for indexColumna, columna in enumerate(fila):
+            if columna:
+                posicion = (indexFila+1,indexColumna+1)
+                posiciones.append(posicion)
 
 
-#print(batallaDeBotes (["b.b..", "b...b",".....","....b"],[(1,1),(3,4),(1,3),(4,5)]))
+    return posiciones
+
+def batallaDeBotes (mapa,posicionADisparar):
+
+    if mapaInvalido(mapa):
+        return[]
+
+    mapaConBooleanos = convertirMapaAMapaBooleano(mapa)
+    disparAlMapa(mapaConBooleanos,posicionADisparar)
+    posicionesBarcosSinHundir = obtenerPosicionesBarcosNoHundidos(mapaConBooleanos)
+
+    return posicionesBarcosSinHundir
+
 
 def ejercicio2(var1,var2):
     return batallaDeBotes(var1,var2)
@@ -65,8 +90,4 @@ assert (ejercicio2(["soy NO valido"],posicionesDeDisparosDePrueba) == [])
 assert (ejercicio2(["yo","tambien","soy","invalido"],posicionesDeDisparosDePrueba) == [])
 assert (ejercicio2(["b.b.","....","..bb","b.b"],posicionesDeDisparosDePrueba) == [])
 assert (ejercicio2(["b.b..","b...b",".....","....b"],posicionesDeDisparosDePrueba) == [(2,1),(2,5)])
-#assert (ejercicio2(["b..","...","..b"],[]) == [(1,1),(3,3)])
-
-
-
-#print (batallaDeBotes(["b.b..","b...b",".....","....b"], posicionesDeDisparosDePrueba))
+assert (ejercicio2(["b..","...","..b"],[]) == [(1,1),(3,3)])
